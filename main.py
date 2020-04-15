c9dc0c62c33e50af4751730442ff0bbd4b79c34d
gjliesen <gjliesen@crimson.ua.edu>
2020-04-15 12:56:29 -0500

    ins dataframe

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import pandas as pd
 import numpy as np
 import math as m
 
+
 def get_data(file):
     df = pd.read_csv(file, index_col=False, squeeze=True)
     return df
+
+
 def get_imu_data(file):
     columns = ['Time','wX','wY','wZ','aX','aY','aZ']
     df = pd.read_csv(file, index_col=False, squeeze=True)
     df.columns = columns
     df = df.set_index('Time')
     return df
+
+
+def init_ins_dataframe(index):
+    columns = ['Lat', 'Long', 'h', 'phi', 'theta', 'psi', 'vX', 'vY', 'vZ']
+    ins_df = pd.DataFrame(index = index, columns=columns)
+    return ins_df
+
 
 def calc_cntb_matrix(phi, theta, psi):
     data = [[np.cos(theta)*np.cos(psi), np.sin(phi)*np.sin(theta)*np.cos(psi) - np.cos(phi)*np.sin(psi),
@@ -20,6 +30,7 @@ def calc_cntb_matrix(phi, theta, psi):
     [-1 * np.sin(theta), np.sin(phi) * np.cos(theta), np.cos(phi) * np.cos(theta)]]
     return pd.DataFrame(data)
 
+
 def calc_a_matrix(phi, theta, psi):
     data = [[1, np.sin(phi)*np.sin(theta), np.cos(phi)*np.sin(theta)],
             [0, np.cos(phi)*np.cos(theta), -1 * np.sin(phi)*np.cos(theta)],
@@ -28,13 +39,15 @@ def calc_a_matrix(phi, theta, psi):
     df *= (1/np.cos(theta))
     return df
 
+
 def main():
     # Parse data
     gps_lla_df = get_data('gps_pos_lla.txt')
     gps_vel_df = get_data('gps_vel_ned.txt')
     imu_df = get_imu_data('imu.txt')
     time_df = get_data('time.txt')
-    euler = [0, 0, 0]
+    ins_df = init_ins_dataframe(imu_df.index)
+
 
 
 
