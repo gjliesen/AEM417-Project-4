8d978321d557a24677d64e4eb707cc43eaf658f1
gjliesen <gjliesen@crimson.ua.edu>
2020-04-15 12:49:14 -0500

    imu interpretation

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ import math as m
 def get_data(file):
     df = pd.read_csv(file, index_col=False, squeeze=True)
     return df
+def get_imu_data(file):
+    columns = ['Time','wX','wY','wZ','aX','aY','aZ']
+    df = pd.read_csv(file, index_col=False, squeeze=True)
+    df.columns = columns
+    df = df.set_index('Time')
+    return df
 
-def calc_c_n_to_b(phi, theta, psi):
+def calc_cntb_matrix(phi, theta, psi):
     data = [[np.cos(theta)*np.cos(psi), np.sin(phi)*np.sin(theta)*np.cos(psi) - np.cos(phi)*np.sin(psi),
              np.cos(phi)*np.sin(theta)*np.cos(psi) + np.sin(phi)*np.sin(psi)],
     [np.cos(theta) * np.sin(psi), np.sin(phi) * np.sin(theta) * np.sin(psi) + np.cos(phi) * np.cos(psi),
@@ -14,12 +20,22 @@ def calc_c_n_to_b(phi, theta, psi):
     [-1 * np.sin(theta), np.sin(phi) * np.cos(theta), np.cos(phi) * np.cos(theta)]]
     return pd.DataFrame(data)
 
+def calc_a_matrix(phi, theta, psi):
+    data = [[1, np.sin(phi)*np.sin(theta), np.cos(phi)*np.sin(theta)],
+            [0, np.cos(phi)*np.cos(theta), -1 * np.sin(phi)*np.cos(theta)],
+            [0, np.sin(phi), np.cos(phi)]]
+    df = pd.DataFrame(data)
+    df *= (1/np.cos(theta))
+    return df
+
 def main():
     # Parse data
     gps_lla_df = get_data('gps_pos_lla.txt')
     gps_vel_df = get_data('gps_vel_ned.txt')
-    imu_df = get_data('imu.txt')
+    imu_df = get_imu_data('imu.txt')
     time_df = get_data('time.txt')
+    euler = [0, 0, 0]
+
 
 
 
