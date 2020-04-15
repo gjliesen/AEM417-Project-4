aecbb65601d3de6ea16fefd56e1ee1b38143e776
gjliesen <gjliesen@crimson.ua.edu>
2020-04-15 12:13:31 -0500

    parsed_data, cntb dataframe

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ def get_data(file):
     df = pd.read_csv(file, index_col=False, squeeze=True)
     return df
 
+def calc_c_n_to_b(phi, theta, psi):
+    data = [[np.cos(theta)*np.cos(psi), np.sin(phi)*np.sin(theta)*np.cos(psi) - np.cos(phi)*np.sin(psi),
+             np.cos(phi)*np.sin(theta)*np.cos(psi) + np.sin(phi)*np.sin(psi)],
+    [np.cos(theta) * np.sin(psi), np.sin(phi) * np.sin(theta) * np.sin(psi) + np.cos(phi) * np.cos(psi),
+     np.cos(phi) * np.sin(theta) * np.sin(psi) - np.sin(phi) * np.cos(psi)],
+    [-1 * np.sin(theta), np.sin(phi) * np.cos(theta), np.cos(phi) * np.cos(theta)]]
+    return pd.DataFrame(data)
 
 def main():
     # Parse data
