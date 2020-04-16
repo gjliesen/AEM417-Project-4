02396f45e596f87118109ba9a71e1be35689c720
gjliesen <gjliesen@crimson.ua.edu>
2020-04-16 13:35:59 -0500

    edits

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+import time
 
+start_time = time.time()
 
 def get_vel_data(file):
     columns = ['vN', 'vE', 'vD']
@@ -28,18 +30,18 @@ def get_imu_data(file):
     return df
 
 
-def initialize_data(imu_df, pos_df):
-    columns=['Time', 'dt', 'lat', 'long', 'h', 'phi', 'theta', 'psi', 'vN', 'vE', 'vD']
-    df = pd.DataFrame(index = imu_df.index, columns=columns)
-    df.Time = imu_df.Time
-    df.dt = df.Time - df.Time.shift(1)
-    df.iloc[0,2] = pos_df.iloc[0,0]
-    df.iloc[0,3] = pos_df.iloc[0,1]
-    df.iloc[0,4] = pos_df.iloc[0,2]
-    df.iloc[0,5:] = 0
-    df = pd.merge(df, imu_df, on='Time')
-
-    return df
+def initialize_ins_data(imu_df, pos_df):
+    columns = ['Time', 'dt', 'lat', 'long', 'h', 'phi', 'theta', 'psi', 'vN', 'vE', 'vD']
+    ins_df = pd.DataFrame(index=imu_df.index, columns=columns)
+    ins_df.Time = imu_df.Time
+    ins_df.dt = ins_df.Time - ins_df.Time.shift(1)
+    ins_df = pd.merge(ins_df, imu_df, on='Time')
+    ins_df.set_index('Time', inplace=True)
+    ins_df.lat.iat[0] = pos_df.lat.iat[0]
+    ins_df.long.iat[0] = pos_df.long.iat[0]
+    ins_df.h.iat[0] = pos_df.h.iat[0]
+    ins_df.iloc[0, 4:10] = 0
+    return ins_df
 
 
 def calc_rn(a, e, l):
@@ -66,7 +68,8 @@ def calc_earth_rotation_matrix(lat):
 
 
 def calc_trans_rate_matrix(rn, re, lat, h, vN):
-    return pd.DataFrame(data=[[vN / (re + h)], [(-1 * vN) / (rn + h)], [(-1 * vN * np.tan(lat))/(re + h)]])
+    return pd.DataFrame(data=[[vN / (re + h)], [(-1 * vN) / (rn + h)],
+                              [(-1 * vN * np.tan(lat))/(re + h)]])
 
 
 def calc_c_bn_matrix(phi, theta, psi):
@@ -87,13 +90,13 @@ def calc_a_nb_matrix(phi, theta):
     return df
 
 
-def attitude_update(ins_df, lat, rn, re, h, vN, phi, theta, psi, wb_ib, c_bn):
+def attitude_update(lat, rn, re, h, vN, phi, theta, wb_ib, c_bn, psi_nb, dt):
     wn_ie = calc_earth_rotation_matrix(lat)
     wn_en = calc_trans_rate_matrix(rn, re, lat, h, vN)
-    wb_in = c_bn@(wn_ie + wn_en)
+    wb_in = c_bn @ (wn_ie.add(wn_en))
     a_nb = calc_a_nb_matrix(phi, theta)
-    wb_nb = wb_ib - wb_in
-    psiDot_nb = a_nb @ wb_nb
+    wb_nb = wb_ib - wb_in.values
+    return (psi_nb + dt * a_nb @ wb_nb).T
 
 
 def calc_ch(a, omega, h, f, u_e):
@@ -112,10 +115,10 @@ def g_n_matrix(f, lat, a, omega_ei, h, u_e):
     return pd.DataFrame(data=[[0],[0],[go * ch]])
 
 
-def velocity_update(f, lat, a, omega_ei, h, u_e, wn_ie, wn_en, c_bn, f_b):
+def velocity_update(f, lat, a, omega_ei, h, u_e, wn_ie, wn_en, c_bn, ins_df, temp):
     g_n = g_n_matrix(f, lat, a, omega_ei, h, u_e)
-    f_n = c_bn@f_b
-    f_n + g_n - get_skew((2 * wn_ie - wn_en))@v_n
+    f_n = c_bn@((ins_df.iloc[temp,11:14]).T)
+    f_n + g_n - get_skew((2 * wn_ie - wn_en))@((ins_df.iloc[temp,8:11]).T)
 
 
 def pe_matrix(rn, re, h, lat):
@@ -129,20 +132,37 @@ def position_update(rn, re, h, lat):
 
 
 def ins_formulation(imu_df, pos_df, vel_df, time_df):
-    ins_df = initialize_data(imu_df, pos_df)
+    ins_df = initialize_ins_data(imu_df, pos_df)
     a = 6378137
     f = 1 / 298.257223563
     e = np.sqrt(f * (2 - f))
-    lat = ins_df.lat.iloc[0]
-    u_e = 3.986004418 * 10**14
-    omega_ei = 7.292115 * 10**(-5)
-    rn = calc_rn(a, e, lat)
-    re = calc_re(a, e, lat)
-    phi = ins_df.iloc[0, 4]
-    theta = ins_df.iloc[0, 5]
-    psi = ins_df.iloc[0, 6]
-    c_bn = calc_c_bn_matrix(phi, theta, psi)
-    print('Done')
+    u_e = 3.986004418 * 10 ** 14
+    omega_ei = 7.292115 * 10 ** (-5)
+
+    flag = True
+    for cur in ins_df.index:
+        if flag:
+            prev = cur
+            flag = False
+        else:
+            lat = ins_df.lat.loc[prev]
+            long = ins_df.long.loc[prev]
+            h = ins_df.h.loc[prev]
+            phi = ins_df.phi.loc[prev]
+            theta = ins_df.theta.loc[prev]
+            psi = ins_df.theta.loc[prev]
+            psi_nb = ins_df.loc[prev, ['phi', 'theta', 'psi']].T
+            wb_ib = ins_df.loc[prev, ['aX', 'aY', 'aZ']].T
+            vN = ins_df.loc[prev, 'vN']
+            dt = ins_df.dt.loc[cur]
+            rn = calc_rn(a, e, lat)
+            re = calc_re(a, e, lat)
+            c_bn = calc_c_bn_matrix(phi, theta, psi)
+            ins_df.loc[cur, ['phi', 'theta', 'psi']] = attitude_update(lat, rn, re, h, vN, phi, theta,
+                                                                       wb_ib, c_bn, psi_nb, dt)
+            prev = cur
+
+
 
 
 
@@ -159,3 +179,4 @@ def main():
 
 
 main()
+print(time.time() - start_time)
